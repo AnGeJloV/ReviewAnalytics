68d1dc4f47febf24757a04718a399d7584ea0f68
Stas Angelov <s.angejlov@gmail.com>
2025-10-21 16:29:30 +0300

    feat: Initialize Spring Boot project with core dependencies

---
+++ src/main/java/com/github/stasangelov/reviewanalytics/ReviewAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.github.stasangelov.reviewanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewAnalyticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/stasangelov/reviewanalytics/ReviewAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stasangelov.reviewanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewAnalyticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
